5f1e8cfcbb825b69b327adfe2e3595f91ef98860
Sumitra <55947732+Ghostz-maker@users.noreply.github.com>
2025-02-03 09:05:43 +0530

    Add files via upload

---
+++ MoonLight.js
@@ -0,0 +1,7 @@
+var key01=document.getElementById("key1"); function key1Sound() {key01.play();}
+var key02=document.getElementById("key2"); function key2Sound() {key02.play();}
+var key03=document.getElementById("key3"); function key3Sound() {key03.play();} 
+var key04=document.getElementById("key4"); function key4Sound() {key04.play();}
+var key05=document.getElementById("key5"); function key5Sound() {key05.play();}
+var key06=document.getElementById("key6"); function key6Sound() {key06.play();}
+var key07=document.getElementById("key7"); function key7Sound() {key07.play();}
